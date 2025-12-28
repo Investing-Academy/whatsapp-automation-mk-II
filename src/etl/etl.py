7d393c940d0992f6dd1d95a674b921b97be4aef6
NVB20 <nivb49@gmail.com>
2025-12-28 12:07:07 +0200

    unconnected sales etl

--- src/etl/etl.py
+++ src/etl/etl.py
@@ -6,7 +6,7 @@ def run_etl():
     # Extract both groups
     extract_result = run_multi_group_reader()
     students_messages = extract_result["students"]
-    sales_messages = extract_result["sales"]
+    #sales_messages = extract_result["sales"]
     
     if len(students_messages) == 0:
         print("=" * 60 + "\n" + "failed to read students messages" + "\n" + "=" * 60)
@@ -14,8 +14,8 @@ def run_etl():
     else:
         run_students_etl(students_messages)
 
-    if len(sales_messages) == 0:
+    #if len(sales_messages) == 0:
         print("=" * 60 + "\n" + "failed to read sales messages" + "\n" + "=" * 60 )
         return
-    else:
+    #else:
         run_sales_etl(sales_messages)
